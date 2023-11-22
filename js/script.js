bd8be009709292c9bcf9d3b6436ddfc5ffaf4b72
otagapgred <137036589+otagapgred@users.noreply.github.com>
2023-11-22 10:00:47 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,26 @@ document.querySelector("#menu-pan").onclick = () => {
 };
 
 //Klik diluar sidebar untuk menghilangkan nav
-const makanan= document.querySelector('#menu-pan')
+const makanan = document.querySelector("#menu-pan");
 
-document.addEventListener('click', function(e){
-    if(!makanan.contains(e.target) && !navbarNav.contains(e.target) )
-    navbarNav.classList.remove('active')
-})
+document.addEventListener("click", function (e) {
+  if (!makanan.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
+
+let stack = document.querySelector(".hero .stack");
+
+[...stack.children].reverse().forEach((i) => stack.append(i));
+
+stack.addEventListener("click", swap);
+
+function swap(e) {
+  let cards = document.querySelector(".cards:last-child");
+  if (e.target !== cards) return;
+  cards.style.animation = "swap 700ms forwards";
+
+  setTimeout(() => {
+    cards.style.animation = "";
+    stack.prepend(cards);
+  }, 700);
+}
